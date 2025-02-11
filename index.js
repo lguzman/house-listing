c89233a502bec07c83b6dcd8c969d80e4bcb447f
Luis Guzman <luismguzman@gmail.com>
2025-02-11 19:52:11 -0400

    Added static HTML template and updated Express to serve it

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const app = express();
+const path = require('path');
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('House Listing API is running');
-});
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
