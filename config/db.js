76ee402fc5d7879c2e561e85f1b5ffd6f568ae44
Luis Guzman <luismguzman@gmail.com>
2025-02-14 14:28:29 -0400

    Updated db.js for Heroku environment variables

--- config/db.js
+++ config/db.js
@@ -1,11 +1,20 @@
+require('dotenv').config();
 const mysql = require('mysql2/promise');
 
-const dbConfig = {
-    host: 'localhost',
-    user: 'root',
-    password: 'UPPR1',
-    database: 'real_estate'
-};
+const dbConfig = process.env.JAWSDB_URL
+    ? {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        port: process.env.DB_PORT || 3306
+      }
+    : {
+        host: 'localhost',
+        user: 'root',
+        password: 'UPPR1',
+        database: 'real_estate'
+      };
 
 const pool = mysql.createPool(dbConfig);
 
